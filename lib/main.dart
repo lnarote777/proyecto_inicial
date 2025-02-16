ec29ba8b93c9d76d8489c8104cefbac5b754d843
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-02-16 21:22:55 +0100

    documentación

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
   runApp(const MyApp());
 }
 
+//Clase principal
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -14,8 +15,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  ThemeMode _themeMode = ThemeMode.light;
+  ThemeMode _themeMode = ThemeMode.light; //Esatado inicial con tema claro
 
+  //Método que alterna entre modo claro y oscuro
   void _toggleTheme() {
     setState(() {
       _themeMode = _themeMode == ThemeMode.light
@@ -29,19 +31,45 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Moodia Demo',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: Colors.green.shade200,
+          onPrimary: Colors.black, 
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.green.shade200,
+          foregroundColor: Colors.black,
+        ),
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: Colors.green.shade200,
+        ),
+      ),
+
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.dark(
+          primary: const Color.fromARGB(255, 20, 119, 144),
+          onPrimary: Colors.white,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: const Color.fromARGB(255, 20, 119, 144),
+          foregroundColor: Colors.white,
+        ),
+        bottomAppBarTheme: BottomAppBarTheme(
+          color: const Color.fromARGB(255, 20, 119, 144),
+        ),
+      ),
       themeMode: _themeMode, //tema dinamico
       home: HomePage(title: 'Moodia Home Page', toggleTheme: _toggleTheme),
     );
   }
 }
 
+//Pagina principal de la aplicación 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title, required this.toggleTheme});
 
   final String title;
-  final VoidCallback toggleTheme;
+  final VoidCallback toggleTheme; //Funcion para cambiar el tema 
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -56,26 +84,27 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
         title: Text(widget.title),
         actions: [
           IconButton(
             icon: const Icon(Icons.brightness_4_rounded),
             onPressed: widget.toggleTheme,
-          ),
+          ), //IconButton para cambiar el tema
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: <Widget> [
+            // Calendario semanal
             TableCalendar(
             firstDay: DateTime.utc(2000, 1, 1),
             lastDay: DateTime.utc(2050, 1, 1),
             focusedDay: _focusedDay,
             calendarFormat: CalendarFormat.week,
             availableCalendarFormats: const { 
-              CalendarFormat.week: 'Semana' // Solo permitir este formato
+              CalendarFormat.week: 'Semana'  //Solo permite el formato de una semana
             },
             daysOfWeekHeight: 40,
             daysOfWeekStyle: DaysOfWeekStyle(
@@ -101,6 +130,7 @@ class _HomePageState extends State<HomePage> {
           ),
           SizedBox(height: 40,),
           Text("Comienza tu camino añadiendo tus emociones del día de hoy.", textAlign: TextAlign.center,),
+          //Imagen dinamica segun el tema
           Image.asset(
             Theme.of(context).brightness == Brightness.light
               ? "assets/moodiaLight.png" // Imagen para tema claro
@@ -108,13 +138,12 @@ class _HomePageState extends State<HomePage> {
             width: 250,
             height: 250,
           ),
-          
-        
           ],
-        
         ),
       ),
+      //Barra de navegación inferior
       bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).bottomAppBarTheme.color, 
         shape: const CircularNotchedRectangle(),
         notchMargin: 8.0,
         child: Row(
@@ -141,6 +170,7 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+      //Botton para agregar una nueva entrada
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
